a70ee359831a328312efa2f50056fc476ce8b9ba
ErikPillon <erik.pillon@gmail.com>
2024-09-01 23:07:44 +0200

    feat: :sparkles: added button to express choice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
+// Get a reference to the button element
+const btnRock = document.getElementById("rock");
+const btnPaper = document.getElementById("paper");
+const btnScissors = document.getElementById("scissors");
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Add an event listener for the 'click' event
+btnRock.addEventListener('click', () => {
+    // Code to be executed when the button is clicked
+    displayMessage(playRound("rock", getComputerChoice()));
+
+    displayScore();
+});
+
+btnPaper.addEventListener('click', () => {
+    // Code to be executed when the button is clicked
+    displayMessage(playRound("paper", getComputerChoice()));
+
+    displayScore();
+});
+
+btnScissors.addEventListener('click', () => {
+    // Code to be executed when the button is clicked
+    displayMessage(playRound("scissors", getComputerChoice()));
+
+    displayScore();
+});
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomChoice = choices[Math.floor(Math.random() * 3)];
@@ -17,12 +47,27 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "scissors" && computerChoice === "paper") ||
         (humanChoice === "paper" && computerChoice === "rock")
     ) {
-        return "You win!" + humanChoice + " beats " + computerChoice;
+        humanScore++;
+        return "You win! " + humanChoice + " beats " + computerChoice;
     } else {
-        return "You lose!" + computerChoice + " beats " + humanChoice;
+        computerScore++;
+        return "You lose! " + computerChoice + " beats " + humanChoice;
     }
 }
 
+function displayMessage(message) {
+    document.getElementById("messageDiv").innerHTML = message;
+}
+
+function displayScore() {
+    document.getElementById("currentScore").innerHTML = "Your score: " + humanScore + " - " + computerScore;
+}
+
+function resetScore() {
+    humanScore = 0;
+    computerScore = 0;
+    displayScore();
+}
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
@@ -54,3 +99,4 @@ function playGame() {
 
     console.log("Final score: " + humanScore + " - " + computerScore);
 }
+
